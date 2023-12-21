2165cf25d01c57a7d2470de69896f5fb4b3d954d
Denis Purtov <a@a.a>
2023-12-21 23:29:28 +0400

    доб фильтры, иконку фильтра, цифру выводимыфх новостей

--- newsportal/news/views.py
+++ newsportal/news/views.py
@@ -21,7 +21,7 @@ def create_article(request):
                 new_article.author = current_user
                 new_article.save() #сохраняем в БД
                 form.save_m2m()
-                # return redirect('news_index')
+                return redirect('news_index')
     else:
         form = ArticleForm()
     return render(request,'news/create_article.html', {'form':form})
@@ -41,9 +41,31 @@ def search_news_auto(request):
 
 
 def news(request):
-    articles = Article.objects.all()
-    context = {'articles':articles}
-    return render(request, 'news/news.html', context)
+
+    categories = Article.categories #создали перечень категорий
+    author_list = User.objects.all() #создали перечень авторов
+    all_articles_len = len(Article.objects.all()) # для цифры отфильтрованных новостей
+    if request.method == "POST":
+        selected_author = int(request.POST.get('author_filter'))
+        selected_category = int(request.POST.get('category_filter'))
+        if selected_author == 0: #выбраны все авторы
+            articles = Article.objects.all()
+        else:
+            articles = Article.objects.filter(author=selected_author)
+        if selected_category != 0: #фильтруем найденные по авторам результаты по категориям
+            articles = articles.filter(category__icontains=categories[selected_category-1][0])
+    else: #если страница открывется впервые
+        selected_author = 0
+        selected_category = 0
+        articles = Article.objects.all()
+
+    context = {'articles': articles, 'author_list':author_list, 'selected_author':selected_author,
+               'categories':categories,'selected_category': selected_category, 'all_articles_len': all_articles_len}
+    
+    return render(request,'news/news_list.html',context)
+
+
+
 
 def news_search(request):
     author_list = User.objects.all()
