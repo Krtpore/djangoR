31493c7d388ef12d38c1fe3f95e5ec8e2f512036
Denis Purtov <a@a.a>
2023-12-22 21:18:34 +0400

    update setting py add allowed hosts

--- newsportal/newsportal/settings.py
+++ newsportal/newsportal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-josyr_bops(pgr4asf-1ybj36&_(fz=9b_pufa=q&1vwaqn*n@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eh0388pe.pythonanywhere.com']
+ALLOWED_HOSTS = ['eh0388pe.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
