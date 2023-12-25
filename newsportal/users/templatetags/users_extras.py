393cafe8cbe0e6ab4c9f4eb8b2577078bfdb743b
Denis Purtov <a@a.a>
2023-12-25 20:48:30 +0400

    create dif menu for dif users

--- newsportal/users/templatetags/users_extras.py
+++ newsportal/users/templatetags/users_extras.py
@@ -3,5 +3,5 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter(name="has_group")
-def has_group(user,group_name):
+def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
